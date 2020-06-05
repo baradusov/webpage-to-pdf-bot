0fbd20419f45fff56ff3c9c8af35c672c8cf5417
Nuril Baradusov <baradusovnh@gmail.com>
2020-06-05 19:47:38 +0400

    Add hindi font support

--- api/index.js
+++ api/index.js
@@ -11,6 +11,8 @@ const FONTS = {
     "https://rawcdn.githack.com/googlefonts/noto-fonts/115d38430d957d38307457c036302b7bdbe0bbc4/unhinted/NotoSansTelugu/NotoSansTelugu-Regular.ttf",
   arabic:
     "https://rawcdn.githack.com/googlefonts/noto-fonts/115d38430d957d38307457c036302b7bdbe0bbc4/unhinted/NotoSansArabic/NotoSansArabic-Regular.ttf",
+  hindi:
+    "https://rawcdn.githack.com/googlefonts/noto-fonts/0723a5939124d6be880d5b8eb4666761bab4235e/unhinted/NotoSerifDevanagari/NotoSerifDevanagari-Regular.ttf",
 };
 const PAGE_STYLE = `
   body { font-size: 2em; }
