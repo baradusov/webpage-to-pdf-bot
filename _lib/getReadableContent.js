e98deba1159c0bfffd55c31097fb3c87376f11cf
Nuril Baradusov <baradusovnh@gmail.com>
2021-05-08 21:58:36 +0400

    Change jsdom user-agent to chrome's one

--- _lib/getReadableContent.js
+++ _lib/getReadableContent.js
@@ -5,6 +5,8 @@ const { Readability } = require('@mozilla/readability');
 module.exports = async (url) => {
   try {
     const resourceLoader = new jsdom.ResourceLoader({
+      userAgent:
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
       strictSSL: false,
     });
     const doc = await JSDOM.fromURL(url, { resources: resourceLoader });
