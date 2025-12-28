57cf963ce577bc6d41cbb899f8bd48935c98960e
Nuril Baradusov <baradusovnh@gmail.com>
2025-12-29 02:01:57 +0400

    refactor getting urls

--- _lib/getUrls.js
+++ _lib/getUrls.js
@@ -1,46 +1,21 @@
-const hasCaptionEntities = (message) => message.caption_entities || null;
-const hasEntities = (message) => message.entities || null;
+const extractUrlsFromEntities = (entities, text) => {
+  if (!entities) return [];
+
+  return entities
+    .filter((entity) => entity.type === 'url' || entity.type === 'text_link')
+    .map((entity) => {
+      if (entity.type === 'text_link') {
+        return entity.url;
+      }
+      return text.substring(entity.offset, entity.offset + entity.length);
+    });
+};
 
 export const getUrls = (message) => {
-  let urls = [];
-
-  if (hasEntities(message)) {
-    const linkEntities = message.entities.filter(
-      (entity) => entity.type === 'url' || entity.type === 'text_link'
-    );
-
-    for (const linkEntity of linkEntities) {
-      if (linkEntity.type === 'url') {
-        urls.push(
-          message.text.substring(
-            linkEntity.offset,
-            linkEntity.offset + linkEntity.length
-          )
-        );
-      } else if (linkEntity.type === 'text_link') {
-        urls.push(linkEntity.url);
-      }
-    }
-  }
-
-  if (hasCaptionEntities(message)) {
-    const linkCaptionEntities = message.caption_entities.filter(
-      (entity) => entity.type === 'url' || entity.type === 'text_link'
-    );
-
-    for (const linkCaptionEntity of linkCaptionEntities) {
-      if (linkCaptionEntity.type === 'url') {
-        urls.push(
-          message.caption.substring(
-            linkCaptionEntity.offset,
-            linkCaptionEntity.offset + linkCaptionEntity.length
-          )
-        );
-      } else if (linkCaptionEntity.type === 'text_link') {
-        urls.push(linkCaptionEntity.url);
-      }
-    }
-  }
+  const urls = [
+    ...extractUrlsFromEntities(message.entities, message.text),
+    ...extractUrlsFromEntities(message.caption_entities, message.caption),
+  ];
 
   return urls.length > 0 ? urls : null;
 };
